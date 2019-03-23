434d790b063eb90b2a13f36bf8e2825f5c9ae309
Michel Antoine Dionne Gutierrez <46821609+SpacecOOk@users.noreply.github.com>
2019-03-23 12:57:28 -0600

    Michel Antoine Dionne Gutierrez A01748632 Grupo:03

---
+++ Mission7.py
@@ -0,0 +1,57 @@
+#Autor: Michel Antoine Dionne Gutierrez A0174863, Grupo: 03
+#Ciclos While
+
+
+def probarDivisiones(divisor, dividiendo):
+    contador = 0
+    while divisor >= dividiendo:
+        divisor = divisor - dividiendo
+        contador = contador + 1
+    print(divisor,"/",dividiendo,"=",contador,"y sobran",divisor)
+
+
+def econtrarMayor():
+    numero = int(input("Teclea un numero[teclea -1 para salir ]"))
+    mayor = 0
+    if numero != -1:
+        while numero != -1:
+            if numero >= 0:
+                if numero >= mayor:
+                    mayor = numero
+            numero = int(input("Teclea un numero[teclea -1 para salir ]"))
+        print(mayor)
+    else:
+        print("No hay valor mayor")
+
+
+def main():
+    print(""" Mission 07. Ciclos while
+              Autor: Michel ANtoine Dionne Gutierrez
+              Matricula: A01748632
+              1.- calcular divisiones
+              2.- encuentra el mayor
+              3.- salir
+              Teclea tu opcion:""")
+    opcion = int(input("Teclea tu opcion"))
+    while opcion != 3:
+        if opcion == 1:
+            divisor = int(input("Teclea el numero que quieres dividir"))
+            dividiendo = int(input("Teclea el numero por el cual quieres dividir"))
+            probarDivisiones(divisor,dividiendo)
+        elif opcion == 2:
+            print("Teclea una serie de valores para encontrar el mayor")
+            econtrarMayor()
+        elif opcion > 3:
+            print("ERROR ,solo valores del 1 al 3")
+        opcion = int(input(""" Mission 07. Ciclos while
+              Autor: Michel ANtoine Dionne Gutierrez
+              Matricula: A01748632
+              1.- calcular divisiones
+              2.- encuentra el mayor
+              3.- salir
+              Teclea tu opcion:"""))
+    print("Gracias por usar este programa, vuelva pronto")
+
+
+
+main()
